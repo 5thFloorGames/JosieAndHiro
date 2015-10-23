55e4429a139e745f39e2ae3bd69916de6997b0e8
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-10-23 15:27:32 +0300

    Clicking on a voiceboxthing makes guards come to it

---
+++ SoundGame/Assets/CameraCast.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCast : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			if(Physics.Raycast(transform.position, Input.mousePosition)){
+				print ("urgblgbl");
+			}
+		}
+	}
+}
---
+++ SoundGame/Assets/MoveWhenSound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWhenSound : MonoBehaviour {
+
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = gameObject.GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void SetDestination(Vector3 destination){
+		agent.destination = destination;
+		agent.Stop();
+		Invoke ("Move", Random.Range(0.2f,1.0f));
+	}
+
+	private void Move(){
+		agent.Resume ();
+	}
+}
