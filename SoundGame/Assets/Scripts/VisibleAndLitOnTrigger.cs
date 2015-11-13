07fd06a5850446821224a85b3689625277187fa5
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-11-13 15:52:11 +0200

    remade the visual puzzle in  total darkness

---
+++ SoundGame/Assets/Scripts/VisibleAndLitOnTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisibleAndLitOnTrigger : MonoBehaviour {
+
+	private Light lightSource;
+
+	void Start () {
+		GetComponent<MeshRenderer> ().enabled = false;
+		lightSource = GetComponentInChildren<Light> ();
+	}
+	
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		gameObject.GetComponent<MeshRenderer> ().enabled = true;
+		gameObject.GetComponent<AudioSource> ().loop = false;
+		lightSource.enabled = true;
+	}
+}
