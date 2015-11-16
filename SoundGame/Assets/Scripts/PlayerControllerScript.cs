c4f924c18dc7e8056e639c66882fe57628ea4d38
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-11-16 11:56:07 +0200

    made Hiro movement and rotation framerate independent

--- SoundGame/Assets/Scripts/PlayerControllerScript.cs
+++ SoundGame/Assets/Scripts/PlayerControllerScript.cs
@@ -3,18 +3,16 @@ using System.Collections;
 
 public class PlayerControllerScript : MonoBehaviour {
 	private float speed = 2.0f;
-	public int maxspeed = 5;
-	private Rigidbody rigid;
 	private bool rotating = false;
 	private bool moving = false;
-	private float rotSpeed = 2.0f;
-	private int frameCounter = 0;
+	public float rotSpeed = 100.0f;
 	private Vector3 target;
 	private Light lightSource;
+	private float rotation = 0.0f;
+	private Quaternion qTo = Quaternion.identity;
 
 	// Use this for initialization
 	void Start () {
-		rigid = GetComponent<Rigidbody> ();
 		lightSource = GetComponentInChildren<Light> ();
 	}
 	
@@ -22,20 +20,22 @@ public class PlayerControllerScript : MonoBehaviour {
 	void Update () {
 		if (!rotating && !moving) {
 			if (Input.GetButton ("Rotate")) {
-				Rotate (3.0f);
+				rotation += 90f;
+				qTo = Quaternion.Euler(0.0f, rotation, 0.0f);
+				Rotate (rotSpeed);
 			} else if (Input.GetButton ("AntiRotate")) {
-				Rotate (-3.0f);
+				rotation -= 90f;
+				qTo = Quaternion.Euler(0.0f, rotation, 0.0f);
+				Rotate (-rotSpeed);
 			}
 		}
 		if (Input.GetButtonDown ("Light")) {
-			StartCoroutine("TurnOffAfterThree");
+			StartCoroutine("TurnOffAfterSecond");
 		}
 
 		if (rotating) {
-			transform.Rotate (0, rotSpeed, 0);
-			frameCounter++;
-			if(frameCounter >= 30){
-				frameCounter = 0;
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, qTo, rotSpeed * Time.deltaTime);
+			if(Quaternion.Angle(transform.rotation, qTo) < 0.1){
 				rotating = false;
 			}
 		}
@@ -43,11 +43,6 @@ public class PlayerControllerScript : MonoBehaviour {
 			float step = speed * Time.deltaTime;
 			transform.position = Vector3.MoveTowards(transform.position, target, step);
 			if(transform.position == target){
-				print (frameCounter);
-			}
-			frameCounter++;
-			if(frameCounter >= 30){
-				frameCounter = 0;
 				moving = false;
 			}
 		}
@@ -68,7 +63,7 @@ public class PlayerControllerScript : MonoBehaviour {
 		}
 	}
 
-	IEnumerator TurnOffAfterThree() {
+	IEnumerator TurnOffAfterSecond() {
 		lightSource.enabled = true;
 		yield return new WaitForSeconds(1);
 		lightSource.enabled = false;
@@ -76,7 +71,6 @@ public class PlayerControllerScript : MonoBehaviour {
 
 	void Rotate(float speed) {
 		rotating = true;
-		rotSpeed = speed;
 	}
 
 	void setTarget(Vector3 transform){
