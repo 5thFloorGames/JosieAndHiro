2e45e7cc9b315bf9035646cb963f742d17ee141a
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-11-24 15:26:35 +0200

    alternate tutorial and placeholder click sound

---
+++ SoundGame/Assets/Scripts/ClickOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickOnTrigger : MonoBehaviour {
+
+	public AudioClip click;
+	private AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+		sound = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		sound.PlayOneShot (click, 1f);
+	}
+	
+}
