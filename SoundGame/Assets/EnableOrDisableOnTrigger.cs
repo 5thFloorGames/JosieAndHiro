3db5fb3781361ec9f6e5d6222ccf012057c692cb
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-11-30 16:05:28 +0200

    Lights off maze for testing

---
+++ SoundGame/Assets/EnableOrDisableOnTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableOrDisableOnTrigger : MonoBehaviour {
+
+	public GameObject[] enablable;
+	public GameObject[] disablable;
+	public bool enable = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Enable(){
+		foreach (GameObject g in enablable) {
+			g.SetActive(true);
+		}
+	}
+	
+	void TurnOff(){
+		foreach (GameObject g in disablable) {
+			g.SetActive(false);
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Hiro" || other.tag == "Josie") {
+			Enable();
+		}
+	}
+}
---
+++ SoundGame/Assets/TurnLightsOn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnLightsOn : MonoBehaviour {
+	
+	[SerializeField]
+	private Light[] lightSources;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		float wait = 0f;
+		foreach (Light lightSource in lightSources) {
+			StartCoroutine(TurnOn(lightSource, wait));
+			wait += 0.3f;
+		}
+	}
+	
+	IEnumerator TurnOn(Light source, float wait){
+		yield return new WaitForSeconds (wait);
+		source.enabled = true;
+	}
+}
