be2ad974aae8d9153a8611f8f539763791eb949e
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-12-08 15:48:19 +0200

    Josie has a model with animations now. Thanks, Omar

--- SoundGame/Assets/Scripts/JosieController.cs
+++ SoundGame/Assets/Scripts/JosieController.cs
@@ -5,11 +5,14 @@ public class JosieController : MonoBehaviour {
 	public float movementSpeed = 10;
 	public float turningSpeed = 60;
 	private AudioSource sound;
-	public AudioClip[] stepSounds;
+	private AudioClip[] stepSounds;
 	public AudioClip signal;
+	private Animator animator;
 
 	void Start(){
 		sound = gameObject.GetComponent<AudioSource> ();
+		stepSounds = Resources.LoadAll<AudioClip>("Audio/Josie/");
+		animator = GetComponent<Animator> ();
 	}
 
 	void Update() {
@@ -19,11 +22,20 @@ public class JosieController : MonoBehaviour {
 		float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
 		transform.Translate(0, 0, vertical);
 
-		if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0){
-			if(!sound.isPlaying){
-				sound.clip = stepSounds[Random.Range(0,stepSounds.Length)];
-				sound.Play();
+		if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0) {
+			if(Input.GetAxis ("Vertical") > 0){
+				animator.SetBool ("Walking", true);
+			} else if (Input.GetAxis ("Vertical") > 0){
+				animator.SetBool ("WalkingBackwards", true);
 			}
+			animator.SetBool ("Walking", true);
+			if (!sound.isPlaying) {
+				sound.clip = stepSounds [Random.Range (0, stepSounds.Length)];
+				sound.Play ();
+			}
+		} else {
+			animator.SetBool ("Walking", false);
+			animator.SetBool ("WalkingBackwards", false);
 		}
 
 		if (Input.GetButton ("Sound")) {
