26b2133d25f1adbd80c494112afe77a3941dd813
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2015-12-10 21:33:27 +0200

    added Hiro checkpoint to the final puzzle

---
+++ SoundGame/Assets/Scripts/MoveCheckpoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCheckpoint : MonoBehaviour {
+
+	public GameObject movableCheckpoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		movableCheckpoint.transform.position = transform.position;
+	}
+
+}
