5a4017caef9fe187c0ca040adac7f14c46ae3ab5
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-08-22 10:50:45 +0300

    grid making and mirroring Hiro movement to another grid

---
+++ SoundGame/Assets/ChangeCubeColor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeCubeColor : MonoBehaviour {
+
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		cube.GetComponent<Renderer> ().material.color = Color.green;
+	}
+
+	void OnTriggerExit(Collider col){
+		cube.GetComponent<Renderer> ().material.color = Color.white;
+	}
+}
---
+++ SoundGame/Assets/CreateGrid.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateGrid : MonoBehaviour {
+
+	public int xAmount = 5;
+	public int yAmount = 5;
+	public Transform secondGrid;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < xAmount; i++){
+			for(int j = 0; j < yAmount; j++){
+				GameObject cube = (GameObject)Instantiate (Resources.Load ("GamePlay Objects/HiroGridBlock"), transform.position + new Vector3 (i, 0, j), Quaternion.identity);
+				cube.transform.parent = transform;
+				GameObject basicCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				basicCube.transform.position = secondGrid.transform.position + new Vector3 (i, 0, j);
+				basicCube.transform.parent = secondGrid;
+				cube.GetComponent<ChangeCubeColor> ().cube = basicCube;
+			}
+		}
+	}
+
+}
