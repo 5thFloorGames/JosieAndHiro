e10b4d35a875b405fa5b3ecec38d89e37ec54609
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-08-25 18:29:33 +0300

    A toggle-switch based version of the audio puzzle

--- SoundGame/Assets/PauseOnTrigger.cs
+++ SoundGame/Assets/PauseOnTrigger.cs
@@ -12,11 +12,11 @@ public class PauseOnTrigger : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
 		transform.Translate(new Vector3(0f,0f,-0.1f));
-		sound.Pause ();
+		sound.Stop ();
 	}
 
 	void OnTriggerExit(Collider other){
 		transform.Translate(new Vector3(0f,0f,0.1f));
-		sound.UnPause ();
+		sound.Play ();
 	}
 }
---
+++ SoundGame/Assets/ToggleSwitch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleSwitch : MonoBehaviour {
+
+	public AudioSource mutable;
+
+	void OnTriggerEnter(Collider other){
+		if (!mutable.mute) {
+			GetComponent<Renderer> ().material.color = Color.green;
+		} else {
+			GetComponent<Renderer> ().material.color = Color.white;
+		}
+		mutable.mute = !mutable.mute;
+		transform.Translate(new Vector3(0f,0f,-0.1f));
+	}
+
+	void OnTriggerExit(Collider other){
+		transform.Translate(new Vector3(0f,0f,0.1f));
+	}
+}
