fc50fcf2a0d570632a6426cd818defdf266cf7ba
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-08-26 17:03:42 +0300

    can now add picture in picture footage of Hiro moving

---
+++ SoundGame/Assets/Scripts/ToggleObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleObject : MonoBehaviour {
+
+	public Behaviour togglable;
+
+	public void Toggle(){
+		togglable.enabled = !togglable.enabled;
+	}
+}
