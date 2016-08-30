843c949a9c667005f5043b17ed0824c280ece527
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-08-30 13:59:02 +0300

    Checkbox technology exists

---
+++ SoundGame/Assets/ToggleOnCondition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToggleOnCondition : MonoBehaviour {
+
+	public Toggle[] conditions;
+	public GameObject togglable;
+
+	public void CheckToggles(){
+		foreach (Toggle condition in conditions) {
+			if (!condition.isOn) {
+				return;
+			}
+		}
+		togglable.SetActive (true);
+	}
+}
