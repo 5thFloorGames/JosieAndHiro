7cabf1bc33fa3393e9d0f9540b95f1365e809839
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-09-07 14:32:54 +0300

    small prototype to test a gauntlet idea and cleaning up assets

---
+++ SoundGame/Assets/Scripts/ToggleMultipleDoors.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleMultipleDoors : MonoBehaviour {
+
+	public DoorOpen[] doors;
+	private AudioSource sound;
+	public Player activatableBy;
+
+	// Use this for initialization
+	void Start () {
+		sound = gameObject.GetComponent<AudioSource> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (checkPlayer (other)) {
+			foreach (DoorOpen door in doors) {
+				door.Open();
+			}		
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (checkPlayer(other)) {
+			foreach (DoorOpen door in doors) {
+				door.Close ();
+			}
+		}
+	}	
+
+	private bool checkPlayer(Collider other){
+		if (activatableBy == Player.Hiro) {
+			return other.tag == "Hiro";
+		} else if (activatableBy == Player.Josie) {
+			return other.tag == "Josie";
+		} else {
+			return (other.tag == "Hiro" || other.tag == "Josie");
+		}
+	}
+}
