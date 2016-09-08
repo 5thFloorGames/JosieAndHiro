7efe4dc40adb1978d01d1a62a45aeb8d4eb225e6
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-09-08 16:53:32 +0300

    reorganising sound and making a new thing that plays them

---
+++ SoundGame/Assets/AdjustableMakeSound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustableMakeSound : MonoBehaviour {
+
+	private AudioClip[] sounds;
+	private AudioSource sound;
+	public string path;
+	public bool playSound = true;
+	public float delay = 0;
+	private bool soundPlaying = false;
+
+	// Use this for initialization
+	void Start () {
+		sounds = Resources.LoadAll<AudioClip>("Audio/" + path);
+
+		sound = GetComponentInChildren<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!sound.isPlaying && !soundPlaying && sound.enabled && playSound) {
+			StartCoroutine(Sound());
+			soundPlaying = true;
+		}
+	}
+
+	IEnumerator Sound(){
+		sound.clip = sounds[Random.Range(0, sounds.Length)];
+		yield return new WaitForSeconds (delay);
+		sound.Play();
+		soundPlaying = false;
+	}
+}
