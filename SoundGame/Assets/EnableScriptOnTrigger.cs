bdbb4d1f8f87990bc028d2a23b73e046527587c9
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-12-02 14:25:05 +0200

    adding new puzzles to puzzle structure and fixing sounds that had gone weird

---
+++ SoundGame/Assets/EnableScriptOnTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableScriptOnTrigger : MonoBehaviour {
+
+	public MonoBehaviour script;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter(Collider col){
+		script.enabled = true;
+	}
+}
